17cb4b58b316b03db6f1de29a6c87f2126773e58
김세린 <68180798+SRin23@users.noreply.github.com>
2021-06-24 12:11:33 +0000

    소수찾기 ver1

---
+++ findPrimeNumber.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 2;i<=n; i++){
+            boolean cnt = true;
+            cnt = true;
+            for(int j = 2; j<i; j++){
+                if(i%j==0){
+                    cnt = false;
+                    break;
+                }
+            }
+            if(cnt){
+                answer++;
+            }
+            
+        }
+        return answer;
+    }
+}
