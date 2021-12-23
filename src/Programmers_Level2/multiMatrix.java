ec0bb8d8147e33f82eb388d95ae05ac98e267194
SRin23 <serin4723@naver.com>
2021-12-23 22:33:06 +0900

    행렬의 곱셈 주석 작성

--- src/Programmers_Level2/multiMatrix.java
+++ src/Programmers_Level2/multiMatrix.java
@@ -26,20 +26,25 @@ public class multiMatrix {
 	}
 
 	public static int[][] solution(int[][] arr1, int[][] arr2) {
-		int[][] answer = new int[arr1.length][arr2[0].length];
-		for (int i = 0; i < arr1.length; i++) {
-			for (int l = 0; l < arr2[0].length; l++) {
-				int temp = 0;
-				for (int j = 0; j < arr1[0].length; j++) {
-					// System.out.print("(" + arr1[i][j] + ", " + arr2[j][l] + ")");
-					temp += arr1[i][j] * arr2[j][l];
-				}
-				// System.out.print("\t");
-				answer[i][l] = temp;
-			}
-			// System.out.println();
-		}
-
+		//행렬의 곱셈으로 만들어진 행렬의 크기는 
+        //행 = 첫번째 배열의 행크기
+        //열 = 두번째 배열의 열크기 와 같다.
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        
+        //첫번째 배열의 행크기만큼 반복
+        for(int i = 0; i<arr1.length; i++){
+        	//두번째 배열의 열크기만큼 반복
+            for(int l = 0; l<arr2[0].length; l++){
+                int temp = 0;
+                //첫번째 배열의 열크기, 두번째 배열의 행크기는 같으며, 이를 반복
+                for(int j = 0; j<arr1[0].length; j++){
+                	//arr1[i][j] * arr2[j][l]의 값을 temp에 더하며 저장(누적합)
+                    temp += arr1[i][j]*arr2[j][l];
+                }
+                //answer[i][l]에 temp 값 저장
+                answer[i][l] = temp;
+            }
+        }
 		return answer;
 
 	}
