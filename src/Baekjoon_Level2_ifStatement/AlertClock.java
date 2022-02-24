0722464fb614676924529c16a53f0ad84f61a62a
SRin23 <serin4723@naver.com>
2022-02-24 23:20:42 +0900

    [백준알고리즘] if문(1)

---
+++ src/Baekjoon_Level2_ifStatement/AlertClock.java
@@ -0,0 +1,25 @@
+package Baekjoon_Level2_ifStatement;
+//알람시계
+import java.util.Scanner;
+
+public class AlertClock {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int hour = sc.nextInt();
+		int minute = sc.nextInt();
+		sc.close();
+		
+		if(minute<45) {
+			hour--;
+			minute = 60-(45-minute);
+			if(hour < 0) {
+				hour = 23;
+			}
+		}else {
+			minute-=45;
+		}
+		System.out.println(hour + " " + minute);		
+	}
+
+}
