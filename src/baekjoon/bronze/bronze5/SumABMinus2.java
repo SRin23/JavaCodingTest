164468993482837b56b556eb2c36678943fb826e
SRin23 <serin4723@naver.com>
2022-03-05 18:28:27 +0900

    [백준 알고리즘] A+B-2

---
+++ src/baekjoon/bronze/bronze5/SumABMinus2.java
@@ -0,0 +1,15 @@
+package baekjoon.bronze.bronze5;
+//A+B-2
+import java.util.Scanner;
+
+public class SumABMinus2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		System.out.println(a+b);
+	}
+
+}
