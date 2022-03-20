963cb26c7d7f834d060d7ab0ea9e6db89bf71d78
SRin23 <serin4723@naver.com>
2022-03-20 16:34:22 +0900

    [백준 알고리즘] 0320

---
+++ src/baekjoon/silver/silver5/FindPrimeNumber.java
@@ -0,0 +1,31 @@
+package baekjoon.silver.silver5;
+//소수찾기
+import java.util.Scanner;
+
+public class FindPrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int result = 0;
+        for(int i = 0; i<n; i++){
+            boolean flag = true;
+            if(arr[i]>1){
+                for(int j = 2; j<arr[i]; j++){
+                    if(arr[i]%j==0){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
